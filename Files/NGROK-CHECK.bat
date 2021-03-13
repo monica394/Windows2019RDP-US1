39cff02f186719d024b2d4a6c38c462c1da58564
monica394 <64655303+monica394@users.noreply.github.com>
2021-03-13 12:24:28 -0500

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -9,7 +9,7 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: Administrator
 echo Pass: Thuonghai001
-curl -O https://github.com/monica394/Windows2019RDP-US1/tree/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -O https://github.com/6Kapp/Windows2019RDP-US1/tree/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://github.com/monica394/Windows2019RDP-US1/tree/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
